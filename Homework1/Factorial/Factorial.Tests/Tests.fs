8432ba97c8bbc2b11bdf887a2ea7183f8833a751
quepum <118292850+quepum@users.noreply.github.com>
2026-02-13 15:44:34 +0300

    style: fix formatting

--- Homework1/Factorial/Factorial.Tests/Tests.fs
+++ Homework1/Factorial/Factorial.Tests/Tests.fs
@@ -7,21 +7,17 @@ open Swensen.Unquote
 open Factorial.Factorial
 
 [<Fact>]
-let ``factorial 0 = 1`` () =
-    test <@ factorial 0 = Some 1I @>
-    
-[<Fact>]
-let ``factorial 1 = 1`` () =
-    test <@ factorial 1 = Some 1I @>
+let ``factorial 0 = 1`` () = test <@ factorial 0 = Some 1I @>
 
 [<Fact>]
-let ``factorial 6 = 720`` () =
-    test <@ factorial 6 = Some 720I @>
+let ``factorial 1 = 1`` () = test <@ factorial 1 = Some 1I @>
 
 [<Fact>]
-let ``factorial negative = None`` () =
-    test <@ factorial -1 = None @>
-    
+let ``factorial 6 = 720`` () = test <@ factorial 6 = Some 720I @>
+
+[<Fact>]
+let ``factorial negative = None`` () = test <@ factorial -1 = None @>
+
 [<Fact>]
 let ``factorial of large n does not hang`` () =
     let result = factorial 100000
--- Homework1/Factorial/Factorial/Library.fs
+++ Homework1/Factorial/Factorial/Library.fs
@@ -1,11 +1,13 @@
 ﻿namespace Factorial
 
 module Factorial =
-    let factorial number: bigint option =
-        if number < 0 then None
-        else    
+    let factorial number : bigint option =
+        if number < 0 then
+            None
+        else
             let rec calc i (acc: bigint) =
                 match i with
                 | _ when i <= 1 -> acc
-                | _ -> calc (i-1) (acc * bigint i)
+                | _ -> calc (i - 1) (acc * bigint i)
+
             Some(calc number (bigint 1))
