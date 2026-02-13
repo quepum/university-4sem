7525b9deacf989aedb5bb3c1b11c44089934071d
quepum <118292850+quepum@users.noreply.github.com>
2026-02-13 15:08:15 +0300

    feat: add factorial implementation

---
+++ Homework1/Factorial/Factorial.Tests/Tests.fs
@@ -0,0 +1,19 @@
+﻿module Tests
+
+open System
+open Factorial
+open Xunit
+open Swensen.Unquote
+open Factorial.Factorial
+
+[<Fact>]
+let ``factorial 0 = 1`` () =
+    test <@ factorial 0 = Some 1I @>
+
+[<Fact>]
+let ``factorial 6 = 720`` () =
+    test <@ factorial 6 = Some 720I @>
+
+[<Fact>]
+let ``factorial negative = None`` () =
+    test <@ factorial -1 = None @>
---
+++ Homework1/Factorial/Factorial/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace Factorial
+
+module Factorial =
+    let factorial number: bigint option =
+        if number < 0 then None
+        else    
+            let rec calc i (acc: bigint) =
+                match i with
+                | _ when i <= 1 -> acc
+                | _ -> calc (i-1) (acc * bigint i)
+            Some(calc number (bigint 1))
