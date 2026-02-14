b74303fa712342a8a0f891f359f33c6eecac0105
quepum <118292850+quepum@users.noreply.github.com>
2026-02-14 13:56:40 +0300

    feat: add fibonacci implementation

---
+++ Homework1/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.fs" author="Alina Letyagina">
+// under MIT License.
+// </copyright>
+
+let fibonacci n =
+    match n with
+    | n when n < 0 -> None
+    | 0 -> Some 0I
+    | 1 -> Some 1I
+    | _ ->
+        let rec calc i (a: bigint) (b: bigint) =
+            if i = n then b else calc (i + 1) b (b + a)
+
+        Some(calc 1 0I 1I)
+
+let n = 9
+
+match fibonacci n with
+| None -> printfn "Invalid argument"
+| Some res -> printfn $"{res}"
