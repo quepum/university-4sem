4202683e5fb73b9da60dd2b21ed554b0d8b0667d
quepum <118292850+quepum@users.noreply.github.com>
2026-02-14 15:56:44 +0300

    feat: add number search implementation

---
+++ Homework1/NumberSearch/NumberSearch/Library.fs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Library.fs" author="Alina Letyagina">
+// under MIT License.
+// </copyright>
+
+namespace NumberSearch
+
+module NumberSearch =
+    let getPosition n ls =
+        let rec calc index =
+            function
+            | [] -> None
+            | head :: tail -> if head = n then Some index else calc (index + 1) tail
+
+        calc 0 ls
