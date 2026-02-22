063b9bfefc2c5f51f5bb4089b039bbb2ded58b00
quepum <118292850+quepum@users.noreply.github.com>
2026-02-22 22:03:00 +0300

    feat: add prime numbers generator

---
+++ Homework2/PrimeNumbers/PrimeNumbers/Library.fs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Library.fs" author="Alina Letyagina">
+// under MIT License.
+// </copyright>
+
+namespace PrimeNumbers
+
+module PrimeNumbers =
+    let generatePrimes =
+        let isPrime n =
+            if n < 2 then
+                false
+            else
+                let max = int (sqrt (float n))
+                [ 2..max ] |> List.forall (fun i -> n % i <> 0)
+
+        seq {
+            yield 2
+            yield! Seq.initInfinite ((+) 3) |> Seq.filter isPrime
+        }
