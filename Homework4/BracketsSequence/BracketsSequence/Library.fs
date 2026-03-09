d8ecd2834526c88df48ba74f9d44e5e9a64fbfbe
quepum <118292850+quepum@users.noreply.github.com>
2026-03-09 16:45:40 +0300

    feat: add Brackets Sequence checker

---
+++ Homework4/BracketsSequence/BracketsSequence/Library.fs
@@ -0,0 +1,29 @@
+﻿namespace BracketsSequence
+
+
+module BracketsSequence =
+    
+    let (|OpenBracket|_|) = function
+        | '(' | '[' | '{' as c -> Some c
+        | _ -> None
+    
+    let (|CloseBracket|_|) = function
+        | ')' -> Some '('
+        | ']' -> Some '['
+        | '}' -> Some '{'
+        | _ -> None
+        
+    let isCorrectSeq str =
+        let rec check chars stack =
+            match chars, stack with
+            | [], [] -> true
+            | [], _ -> false
+            | OpenBracket ch :: tail, stack -> check tail (ch :: stack)
+            | CloseBracket ch :: rest, top :: tail when top = ch -> check rest tail
+            | CloseBracket _ :: _, _ -> false
+            | _ :: rest, stack -> check rest stack
+        check (List.ofSeq str) []
+       
+            
+        
+    
