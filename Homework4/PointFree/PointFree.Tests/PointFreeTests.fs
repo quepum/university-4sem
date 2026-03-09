b5b6a2162ae7f0fc5fa97e5afe534ccbac5fc35d
quepum <118292850+quepum@users.noreply.github.com>
2026-03-09 12:21:15 +0300

    feat: add PointFree solution

---
+++ Homework4/PointFree/PointFree.Tests/PointFreeTests.fs
@@ -0,0 +1,12 @@
+﻿module PointFree.Tests
+
+
+open FsCheck
+open NUnit.Framework
+open PointFree
+
+[<Test>]
+let ``point-free func equals original`` () =
+    let property x (l: int list) =
+        func'4 x l = List.map (fun y -> y * x) l
+    Check.QuickThrowOnFailure property
---
+++ Homework4/PointFree/PointFree/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace PointFree
+
+module PointFree =
+    let func x l = List.map (fun y -> y * x) l
+    
+    let func'1 x =  List.map (fun y -> y * x)
+    
+    let func'2 x = List.map ( (*) x)
+    
+    let func'3 x = (List.map << (*)) x
+    
+    let func'4 = List.map << (*)
