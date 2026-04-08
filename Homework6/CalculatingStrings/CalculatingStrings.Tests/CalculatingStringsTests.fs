d8c3aa4290c0fbc0810e4bd4c5a464e417178745
quepum <118292850+quepum@users.noreply.github.com>
2026-04-08 23:56:26 +0300

    style: fix formatting

--- Homework6/CalculatingStrings/CalculatingStrings.Tests/CalculatingStringsTests.fs
+++ Homework6/CalculatingStrings/CalculatingStrings.Tests/CalculatingStringsTests.fs
@@ -12,41 +12,45 @@ let calculate = CalculatingStringsBuilder()
 
 [<Test>]
 let ``Calculate workflow returns Some when all strings are valid integers`` () =
-    test <@ 
-        calculate {
-            let! x = "1"
-            let! y = "2"
-            let z = x + y
-            return z
-        } = Some 3 
-    @>
+    test
+        <@
+            calculate {
+                let! x = "1"
+                let! y = "2"
+                let z = x + y
+                return z
+            } = Some 3
+        @>
 
 [<Test>]
 let ``Calculate workflow returns None when any string is invalid`` () =
-    test <@ 
-        calculate {
-            let! x = "1"
-            let! y = "."
-            let z = x + y
-            return z
-        } = None 
-    @>
+    test
+        <@
+            calculate {
+                let! x = "1"
+                let! y = "."
+                let z = x + y
+                return z
+            } = None
+        @>
 
 [<Test>]
 let ``Calculate workflow handles empty string as error`` () =
-    test <@ 
-        calculate {
-            let! x = ""
-            return x
-        } = None 
-    @>
+    test
+        <@
+            calculate {
+                let! x = ""
+                return x
+            } = None
+        @>
 
 [<Test>]
 let ``Calculate workflow handles negative numbers correctly`` () =
-    test <@ 
-        calculate {
-            let! x = "-5"
-            let! y = "10"
-            return x + y
-        } = Some 5 
-    @>
+    test
+        <@
+            calculate {
+                let! x = "-5"
+                let! y = "10"
+                return x + y
+            } = Some 5
+        @>
